f2c409b2e408e713e4c7d6b88cacb48a39832b2f
byungKHee <byungheekang@g.skku.edu>
2025-07-24 19:44:33 +0900

    chore: 스프링 프로젝트 생성

---
+++ sosd-backend/src/main/java/com/sosd/sosd_backend/SosdBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sosd.sosd_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SosdBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SosdBackendApplication.class, args);
+	}
+
+}
---
+++ sosd-backend/src/test/java/com/sosd/sosd_backend/SosdBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sosd.sosd_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SosdBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
