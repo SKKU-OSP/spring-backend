f851c2c2187d4823e4e46dd72a07b2f673790753
byungKHee <byungheekang@g.skku.edu>
2025-08-05 23:39:07 +0900

    feat: 레포 수집 테스트 코드 작성

--- sosd-backend/src/test/java/com/sosd/sosd_backend/github_collector/RepoCollectTest.java
+++ sosd-backend/src/test/java/com/sosd/sosd_backend/github_collector/RepoCollectTest.java
@@ -1,10 +1,14 @@
-package com.sosd.sosd_backend;
+package com.sosd.sosd_backend.github_collector;
 
-import com.sosd.sosd_backend.github_collector.dto.TestRepoDto;
+import com.sosd.sosd_backend.dto.RepositoryDetailDto;
 import com.sosd.sosd_backend.github_collector.collector.RepoCollector;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class RepoCollectTest {
@@ -14,7 +18,20 @@ public class RepoCollectTest {
 
     @Test
     void testGetRepoInfo(){
-        TestRepoDto testRepoDto = repoCollector.getRepoInfo("SKKU-OSP", "SKKU-OSP");
+        RepositoryDetailDto testRepoDto = repoCollector.getRepoInfo("SKKU-OSP", "SKKU-OSP");
+
+        assertEquals(503242724L, testRepoDto.id(), "Repo ID가 일치하지 않습니다");
+        assertEquals(107451259L, testRepoDto.owner().id(), "Owner ID가 일치하지 않습니다");
+
+        // 디버깅용 출력
         System.out.println(testRepoDto);
+    }
+
+    @Test
+    void testGetAllReposFromUser(){
+        List<RepositoryDetailDto> repoLists = repoCollector.getAllContributedRepos("ki011127");
+        for(RepositoryDetailDto repo : repoLists){
+            System.out.println(repo.fullName());
+        }
     }
 }
