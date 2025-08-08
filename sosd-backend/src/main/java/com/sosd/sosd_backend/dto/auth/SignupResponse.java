ac4fa7eac48885253e39f6dc93c7722b49851ba6
ki011127 <ki011127@naver.com>
2025-08-08 16:48:26 +0900

    feat: 회원가입 api 구현

---
+++ sosd-backend/src/main/java/com/sosd/sosd_backend/dto/auth/SignupResponse.java
@@ -0,0 +1,23 @@
+package com.sosd.sosd_backend.dto.auth;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
+public class SignupResponse {
+    private String studentId;
+    private String name;
+    private String college;
+    private String dept;
+    private int pluralMajor;
+    private int absence;
+    private LocalDateTime dateJoined;
+
+    private Long githubId;
+    private String githubLoginUsername;
+    private String githubName;
+    private String githubEmail;
+}
