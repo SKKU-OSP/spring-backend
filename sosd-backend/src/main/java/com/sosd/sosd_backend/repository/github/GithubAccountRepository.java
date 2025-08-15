7999e94d3d2b309690652a9cc6992f9755efe6bb
byungKHee <byungheekang@g.skku.edu>
2025-08-15 17:09:52 +0900

    feat: github 중복 가입 체크

--- sosd-backend/src/main/java/com/sosd/sosd_backend/repository/github/GithubAccountRepository.java
+++ sosd-backend/src/main/java/com/sosd/sosd_backend/repository/github/GithubAccountRepository.java
@@ -4,4 +4,6 @@ import com.sosd.sosd_backend.entity.github.GithubAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface GithubAccountRepository extends JpaRepository<GithubAccount, Long> {
+    // PK로 조회
+    boolean existsByGithubId(Long githubId);
 }
