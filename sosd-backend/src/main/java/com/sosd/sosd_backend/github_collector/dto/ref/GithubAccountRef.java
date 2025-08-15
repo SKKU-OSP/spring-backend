517cc5858f09ffd46578d55b5cbf59a9bb355675
byungKHee <byungheekang@g.skku.edu>
2025-08-15 19:53:55 +0900

    feat: 수집기 내부에서 사용하는 참조 dto 추가

---
+++ sosd-backend/src/main/java/com/sosd/sosd_backend/github_collector/dto/ref/GithubAccountRef.java
@@ -0,0 +1,25 @@
+package com.sosd.sosd_backend.github_collector.dto.ref;
+
+import java.util.Objects;
+
+/**
+ * 깃허브 수집 로직에서 사용할 github 계정 잠조 정보 DTO
+ *
+ * @param githubId
+ * @param githubLoginUsername
+ * @param githubName
+ * @param githubToken
+ * @param githubEmail
+ */
+public record GithubAccountRef(
+        Long githubId,
+        String githubLoginUsername,
+        String githubName,
+        String githubToken,
+        String githubEmail
+) {
+    public GithubAccountRef{
+        Objects.requireNonNull(githubId);
+        Objects.requireNonNull(githubLoginUsername);
+    }
+}
