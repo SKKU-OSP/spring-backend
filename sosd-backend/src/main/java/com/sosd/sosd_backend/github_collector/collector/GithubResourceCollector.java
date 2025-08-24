a602cbd4083f8d07b9fd8a0ebd80cafd45882bfa
byungKHee <byungheekang@g.skku.edu>
2025-08-25 02:12:20 +0900

    fix: 불필요한 cursor 파라미터 삭제

--- sosd-backend/src/main/java/com/sosd/sosd_backend/github_collector/collector/GithubResourceCollector.java
+++ sosd-backend/src/main/java/com/sosd/sosd_backend/github_collector/collector/GithubResourceCollector.java
@@ -3,8 +3,9 @@ package com.sosd.sosd_backend.github_collector.collector;
 import com.sosd.sosd_backend.github_collector.dto.collect.context.CollectContext;
 import com.sosd.sosd_backend.github_collector.dto.collect.result.CollectResult;
 import com.sosd.sosd_backend.github_collector.dto.collect.result.Cursor;
+import org.springframework.stereotype.Component;
 
 public interface GithubResourceCollector<CC extends CollectContext, T, C extends Cursor> {
-    CollectResult<T,C> collect(CC collectContext, C cursor);
+    CollectResult<T,C> collect(CC collectContext);
     default String source() { return "generic"; }
 }
