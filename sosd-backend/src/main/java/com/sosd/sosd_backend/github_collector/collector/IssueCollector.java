94d6181946fec2152018fed15eba7077513f42c6
byungKHee <byungheekang@g.skku.edu>
2025-08-26 14:33:48 +0900

    fix: 날짜 처리 방식 수정

--- sosd-backend/src/main/java/com/sosd/sosd_backend/github_collector/collector/IssueCollector.java
+++ sosd-backend/src/main/java/com/sosd/sosd_backend/github_collector/collector/IssueCollector.java
@@ -50,11 +50,11 @@ public class IssueCollector implements GithubResourceCollector
         final String repoFullName = ctx.repoRef().fullName();               // e.g. "SKKU-OSP/spring-backend"
         final String author = ctx.githubAccountRef().githubLoginUsername(); // e.g. "byungKHee"
         final OffsetDateTime since = ctx.lastIssueDate() == null
-                ? OffsetDateTime.parse("2018-12-31T59:59:59Z")
+                ? OffsetDateTime.parse("2018-12-31T23:59:59Z")
                 : ctx.lastIssueDate();                   // 시작 시간, null이면 2019년부터 수집
 
 
-        String q = "repo:%s is:issue author:%s created:>%s".formatted(repoFullName,author,since.toString());
+        String q = "repo:%s is:issue author:%s created:>%s".formatted(repoFullName,author,since.toInstant().toString());
         int pageSize = 100;
 
         List<GithubIssueResponseDto> issueResults = new ArrayList<>();
